0c572d1233383f84b75c8e0db0b789dbd49d0166
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-10-25 08:05:51 -0300

    Criando Form de Categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/categories", (req,res) =>{
-    res.send("ROTA DE CATEGORIAS")
-})
-
-router.get("/admin/categories/new", (req, res) =>{
-    res.send("Rota pra criar nova categoria")
+router.get("/admin/categories/new" , (req,res) => {
+    res.render("admin/categories/new")
 })
 
 module.exports = router
