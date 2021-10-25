718068b2193938f2349ea688310454580be38010
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-10-25 08:19:25 -0300

    Cadastro de Categorias com slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,24 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new" , (req,res) => {
     res.render("admin/categories/new")
 })
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title) //biblioteca que transforma o titulo em slug , ex sao-paulo
+        }).then(()=>{
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports = router
