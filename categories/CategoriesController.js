4f2d259b8050bab2b64a665153c694a630f4d878
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-11-08 08:13:54 -0300

    Formulario de edição de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title) //biblioteca que transforma o titulo em slug , ex sao-paulo
         }).then(()=>{
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     }else{
         res.redirect("/admin/categories/new");
@@ -42,6 +42,23 @@ router.post("/categories/delete", (req, res) => {
             res.redirect("/admin/categories")
         }
    }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories")   
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render("admin/categories/edit", {category: category})
+        }else{
+            res.redirect("/admin/categories")   
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories") 
+    })
 })
 
 module.exports = router
