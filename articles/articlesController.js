1caa9ae2372d389539876b4bc7e072b2decfa998
fetin3 <7822551-FelipeTintori@users.noreply.gitlab.com>
2021-11-19 08:58:00 -0300

    slavando edição e formulario de articles

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -6,7 +6,7 @@ router.get("/articles", (req,res) =>{
 })
 
 router.get("/admin/articles/new", (req, res) =>{
-    res.send("Rota pra criar nova categoria")
+    res.render("admin/articles/new")
 })
 
 module.exports = router
